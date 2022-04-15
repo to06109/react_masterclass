a24263710e1bbfd399ee8d9b671ec5ade5d49170
Yesong <to06109@naver.com>
2022-04-15 13:06:46 +0900

    7.6 Animation gesture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
+import { useRef } from 'react'
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,66 +10,48 @@ const Wrapper = styled.div`
   align-items: center;
 `
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  // 네모상자 을 grid형식으로 구성
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  // 원이 grid의 중앙에 오게 함
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    }
-  }
+  hover: { scale: 1.5, rotateZ: 90 },
+  tap:{scale: 1, borderRadius: "100px"},
+
 }
 
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  }, 
-  end: {
-    opacity: 1,
-    y: 0,
-  }
-}
 
 function App() {
+  // 코드로 특정 element 잡아옴
+  const biggerBoxRef = useRef<HTMLDivElement>(null)
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants}/>
-        <Circle variants={circleVariants}/>
-        <Circle variants={circleVariants}/>
-        <Circle variants={circleVariants}/>
-      </Box>
+      <BiggerBox ref={biggerBoxRef}>
+      <Box 
+        drag
+        dragSnapToOrigin
+        dragElastic={0.5}
+        dragConstraints={biggerBoxRef}
+        variants={boxVariants}
+        whileHover= "hover"
+        whileTap= "tap"
+      />
+      </BiggerBox>
     </Wrapper>
   )
 }
