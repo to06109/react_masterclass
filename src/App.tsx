cd702b82cfce3eb27c6f68b6aed26386a4076fa0
Yesong <to06109@naver.com>
2022-04-15 18:08:21 +0900

    7.13(1) Animation - Slider 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,44 +8,66 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `
 const Box = styled(motion.div)`
   width: 400px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 35px;
   position: absolute;
-  top: 100px;
+  top: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
-const boxVatiants = {
-  // 나타날 때: initial -> visible
-  // 사라질 때: visible -> leaving
-  initial: {
+const box = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 1, 
+    }
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 20,
-  }
+    transition: {
+      duration: 1, 
+    }
+  },
 }
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing(prev => !prev)
+  const [visible, setVisible] = useState(1)
+  const nextPlease = () => setVisible(prev => prev === 10 ? 10 : prev + 1)
+  const prevPlease = () => setVisible(prev => prev === 1 ? 1 : prev - 1)
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-      <AnimatePresence>{showing ? <Box variants={boxVatiants} initial="initial" animate="visible" exit="leaving"/> : null}</AnimatePresence>
+      <AnimatePresence>
+        <Box 
+          variants={box} 
+          initial="invisible" 
+          animate="visible" 
+          exit="exit" 
+          key={visible}>{visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   )
 }
+
+// # 7.13 2: 36
 
 export default App
